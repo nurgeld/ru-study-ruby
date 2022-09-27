7dbe5e37f031b2d0d2b65665677ffd3ff20481dd
nurgeld <nurgeldy@gmail.com>
2022-09-28 01:08:38 +0600

    solved first exercise in array task

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,8 +1,18 @@
 module Exercise
   module Arrays
     class << self
+      def qsort(array)
+        return [] if array.empty?
+        pivot = array.first
+        rest = array[1..-1]
+        left = rest.select { |el| el <= pivot }
+        right = rest.select { |el| el > pivot }
+        qsort(left).concat([pivot], qsort(right)) 
+      end
+
       def replace(array)
-        array
+        max = qsort(array).last
+        array.map { |el| el.positive? ? max : el }
       end
 
       def search(_array, _query)
