287d7dc549e156c9bae28ae720f7e0983a8ab2da
nurgeld <nurgeldy@gmail.com>
2022-09-29 00:14:46 +0600

    solved all exercises in array task

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -16,8 +16,23 @@ module Exercise
         array.map { |el| el.positive? ? max : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        def bin_search(arr, low, high, query)
+          if high >= low
+            mid = (high + low) / 2
+            return mid if arr[mid] == query
+
+            if arr[mid] > query
+              bin_search(arr, low, mid - 1, query)
+            else
+              bin_search(arr, mid + 1, high, query)
+            end
+          else
+            -1
+          end
+        end
+
+        bin_search(array, 0, array.size - 1, query)
       end
     end
   end
