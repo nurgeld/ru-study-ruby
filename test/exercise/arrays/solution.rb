36a72ae303a134f765ef797facb160bd422f7d84
nurgeld <nurgeldy@gmail.com>
2022-09-30 23:34:23 +0600

    fix search function implementation

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -12,10 +12,9 @@ module Exercise
         array.map { |el| el.positive? ? max : el }
       end
 
-      def search(array, query, low = 0, high = nil)
-        high = array.size - 1 if high.nil?
-
-        return -1 if low > high
+      def search(array, query, low = 0, high = array.size - 1)
+        uniq_items = array.to_set
+        return -1 if uniq_items.size != uniq_items.add(query).size
 
         mid = (low + high) / 2
         return mid if query == array[mid]
