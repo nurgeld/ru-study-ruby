6defec93ffd5a5b0fba8ca51294143112a82bc54
nurgeld <nurgeldy@gmail.com>
2022-10-04 00:16:02 +0600

    add function for calculate avg rating of films

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,17 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtered_films = array.select do |row|
+          !row['rating_kinopoisk'].nil? &&
+            row['rating_kinopoisk'] != '0' &&
+            !row['country'].nil? &&
+            row['country'].split(',').size >= 2
+        end
+        films_count = filtered_films.size
+
+        sum_of_ratings = filtered_films.map { |row| row['rating_kinopoisk'].to_f }.reduce(:+)
+        sum_of_ratings / films_count
       end
 
       def chars_count(_films, _threshold)
